88e9fcb324e751b25b8f55339c6f179ff4380c60
郭呈程 <guochengcheng@cloudwalk.cn>
2020-01-10 21:16:42 +0800

    init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# @sagacious/template-package
+
+![logo](https://sagacious-1300640815.cos.ap-beijing.myqcloud.com/Donkey_128px_1082061_easyicon.net.ico)
+
+**🐴 a universal customized npm package development template.**
+
+## Features
+
+### The following environments are pre-integrated
+
+- typescript
+- rollup
+- jest
+- eslint
+- more...
+
+### Eslint fix is enabled by default
+
+## Related
+
+- [@sagacious/cli](https://www.npmjs.com/package/@sagacious/cli) 🐎 a universal template management tool
+- [eslint-config-sagas](https://www.npmjs.com/package/eslint-config-sagas) a public eslint configuration that fits most project styles
+- more...
---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+const { eslint } = require('rollup-plugin-eslint')
+const json = require('rollup-plugin-json')
+const ts = require('@rollup/plugin-typescript')
+const del = require('rollup-plugin-delete')
+const fs = require('@sagacious/fs-wrapper')
+const path = require('path')
+
+const libOptions = fs.readDirProSync(path.resolve(__dirname, './src'), {
+  deep: true,
+  withFileTypes: true,
+})
+  .filter(f => /\.ts$/.test(f.absolutePath))
+  .map(f => ({
+    input: f.absolutePath,
+    output: {
+      file: `./lib/${f.name.replace(/\.ts$/, '')}.js`,
+      format: 'cjs',
+    },
+    plugins: [
+      json(),
+      eslint({
+        fix: true,
+      }),
+      ts({
+        module: 'CommonJS',
+      }),
+    ],
+  }))
+
+module.exports = [
+  {
+    input: './src/index.ts',
+    output: {
+      file: './dist/index.js',
+      format: 'cjs',
+    },
+    plugins: [
+      del({
+        targets: [
+          './dist/*',
+          './lib/*',
+        ],
+      }),
+      json(),
+      ts(),
+    ],
+  },
+  ...libOptions,
+]
